9ae86841395db6c9613bb00b26646e903402abee
GAKU WATANABE <watanabe.kk25024@kaien-t4.net>
2026-04-27 14:12:33 +0900

    settings.py を編集

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "login"
