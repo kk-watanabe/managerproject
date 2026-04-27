9b5e7dbc5619d7840b5e3a19df052e8e436b932b
GAKU WATANABE <watanabe.kk25024@kaien-t4.net>
2026-04-27 16:30:09 +0900

    settings.py を編集

--- config/settings.py
+++ config/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "questdb",
+        "USER": "postgres",
+        "PASSWORD": "nicola138",
+        "HOST": "questdb.cr0yqauyup51.ap-northeast-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
 
