d7e5a1701c008b48b4be417fc6ebae05c8ddcf72
kk-watanabe <yuewatanabe.math@gmail.com>
2026-05-01 23:22:43 +0900

    260501

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -88,13 +88,7 @@ DATABASES = {
         "PORT": "5432",
     }
 }
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
